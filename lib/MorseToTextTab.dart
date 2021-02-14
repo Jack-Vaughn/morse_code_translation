275f9892ca0b0a93f6e6a415a7e98db6433dd463
Jack Vaughn <jack.vaughn0523@gmail.com>
2021-02-14 13:00:47 -0500

    phase-i starting template

---
+++ lib/MorseToTextTab.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MorseToTextTab extends StatefulWidget {
+  @override
+  _DeckViewState createState() => _DeckViewState();
+}
+
+class _DeckViewState extends State<MorseToTextTab> {
+  // DECLARE VARIABLES HERE
+
+  _DeckViewState() {
+    // PLACEHOLDER
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // PLACEHOLDER
+    return Text('Imma placeholder');
+  }
+}
---
+++ lib/TextToMorseTab.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TextToMorseTab extends StatefulWidget {
+  @override
+  _DeckViewState createState() => _DeckViewState();
+}
+
+class _DeckViewState extends State<TextToMorseTab> {
+  // DECLARE VARIABLES HERE
+
+  _DeckViewState() {
+    // PLACEHOLDER
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // PLACEHOLDER
+    return Text('Imma placeholder');
+  }
+}
