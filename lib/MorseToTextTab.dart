8903279cc3f637525edc50ec2333b9c0f096df9f
Adam Cunningham <adamcunningham54@gmail.com>
2021-02-16 16:18:15 -0500

    Added textfields to the M2T tab and gave functionality to the Clear Button

--- lib/MorseToTextTab.dart
+++ lib/MorseToTextTab.dart
@@ -7,14 +7,61 @@ class MorseToTextTab extends StatefulWidget {
 
 class _DeckViewState extends State<MorseToTextTab> {
   // DECLARE VARIABLES HERE
+  final nameHolder = TextEditingController();
 
-  _DeckViewState() {
-    // PLACEHOLDER
-  }
+  clearTextInput(){
+    nameHolder.clear();
+  }//end clearTextInput
 
   @override
   Widget build(BuildContext context) {
-    // PLACEHOLDER
-    return Text('Imma placeholder');
+    // Basic material design visual layout structure
+    return Scaffold(
+      // Centers content horizontally and vertically
+        body: Center(
+            child: Container(
+              // Offsets in each of the two horizontal directions
+                margin: new EdgeInsets.symmetric(horizontal: 20.0),
+                child: Center(
+                  // Displays children in a vertical array
+                    child: Column(
+                      // Place the children as close to the middle of the main axis as possible
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Input Morse Code Here',
+                            contentPadding: const EdgeInsets.all(20.0),
+                          ),
+                          controller: nameHolder,
+                          style: new TextStyle(
+                              fontSize: 34.0,
+                              color: const Color(0xFF000000),
+                              fontWeight: FontWeight.w200,
+                              fontFamily: "Roboto"),
+                        ),
+                        ButtonBar(
+                          children: <Widget>[
+                            RaisedButton(
+                              child: Text("Submit"),
+                              onPressed: () => {},
+                              color: Colors.indigo,
+                              textColor: Colors.white,
+                              padding: EdgeInsets.all(2.0),
+                              splashColor: Colors.orange,
+                            ),
+                            RaisedButton(
+                              child: Text("Clear"),
+                              color: Colors.indigo,
+                              onPressed: clearTextInput,
+                              textColor: Colors.white,
+                              padding: EdgeInsets.all(2.0),
+                              splashColor: Colors.orange,
+                            ),
+                          ],
+                        )
+                      ],
+                    )))));
   }
 }
